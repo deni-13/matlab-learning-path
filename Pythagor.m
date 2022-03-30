e5a8f9956e690b617e580eed6278faacdde9e092
deniz-13 <d.akkas35@gmail.com>
2022-03-30 12:57:37 +0300

    trial

---
+++ Pythagor.m
@@ -0,0 +1,7 @@
+% A Pythagorean triplet is a set of three natural numbers, abc, for which,
+%  a"2 + b^2 - c2
+% For example
+%  3*2 + 42 -9+ 16 - 52 - 25.
+% There exists exactly ane Pythagorean triplet for which a + b+c= N (the input)
+% Find the product abc
+% The name of your tunction should be Pythagor
